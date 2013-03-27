3fec0c8f8cca88339485c0ddaf6190df13078151
Diego Dias <emerson.diego.duarte@gmail.com>
2013-03-27 14:32:37 -0300

    Revert "Revert "Revert "Commit CDI e Hibernate/JPA"""
    This reverts commit 868c316631cea70781146f055e378ba0e395d275.

--- src/br/com/services/business/EstabelecimentoBusinessImpl.java
+++ src/br/com/services/business/EstabelecimentoBusinessImpl.java
@@ -7,34 +7,32 @@ import javax.inject.Inject;
 import br.com.services.dao.EstabelecimetoDaoImpl;
 import br.com.services.model.Estabelecimento;
 
-public class EstabelecimentoBusinessImpl  {
+public class EstabelecimentoBusinessImpl implements
+		EstabelecimentoBusinessInterface {
 
 	@Inject
 	private EstabelecimetoDaoImpl estabelecimentoDAO;
 
 	
 
-	
+	@Override
 	public List<Estabelecimento> getEstabelecimentos() {
 		return estabelecimentoDAO.getEstabelecimentos();
 	}
 
-	
+	@Override
 	public Estabelecimento getEstabelecimentoPorId(int id) {
 		return estabelecimentoDAO.getEstabelecimentoPorId(id);
 	}
 
-	
+	@Override
 	public String adicionarEstabelecimento(Estabelecimento estabelecimento) {
 		return estabelecimentoDAO.adicionarEstabelecimento(estabelecimento);
 	}
 
-	
+	@Override
 	public String atualizarEstabelecimento(Estabelecimento estabelecimento) {
 		return estabelecimentoDAO.atualizarEstabelecimento(estabelecimento);
 	}
-
-	
-	
 
 }
--- src/br/com/services/dao/EstabelecimetoDaoImpl.java
+++ src/br/com/services/dao/EstabelecimetoDaoImpl.java
@@ -2,39 +2,30 @@ package br.com.services.dao;
 
 import java.util.List;
 
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-
 import br.com.services.model.Estabelecimento;
 
-public class EstabelecimetoDaoImpl  {
-	
-	public EstabelecimetoDaoImpl() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	@Inject
-	private EntityManager entityManager;
+public class EstabelecimetoDaoImpl implements EstabelecimetoDaoInterface {
 
+	@Override
 	public List<Estabelecimento> getEstabelecimentos() {
-		Query query = entityManager.createQuery("select e from Estabelecimento e");
-		List<Estabelecimento> estabelecimentos = (List<Estabelecimento>) query.getResultList();
-		return estabelecimentos;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
+	@Override
 	public Estabelecimento getEstabelecimentoPorId(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	
+	@Override
 	public String adicionarEstabelecimento(Estabelecimento estabelecimento) {
 		return null;
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
 	public String atualizarEstabelecimento(Estabelecimento estabelecimento) {
 		return null;
 		// TODO Auto-generated method stub
